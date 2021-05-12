ab63e2a8fd193d7103ae419c510ef7e7de001971
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-12 18:10:28 +0300

    ...

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ console.log(sideFlwr.getTotalLength())
 $(window).on('load', () => {
   var timeline = gsap.timeline()
     .to(sideFlwr, {
-      strokeDashoffset: 0, duration: 8, ease: 'circ.easeIn'
+      strokeDashoffset: 0, duration: 4000, ease: 'circ.easeIn'
     })
-    .fromTo(sideFlwr,{fill: 'none'}, {fill: "#355070"},"<1")
+    .fromTo(sideFlwr,{fill: 'none'}, {fill: "#355070"})
 })
