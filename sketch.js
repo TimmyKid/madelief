6edd12daa5505fd38832721ea2b728cf2913969f
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-12 23:17:36 +0300

    progress not perfection

--- sketch.js
+++ sketch.js
@@ -2,9 +2,21 @@ const sideFlwr = document.querySelector('#sideFlowr path');
 console.log(sideFlwr.getTotalLength())
 
 $(window).on('load', () => {
-  var timeline = gsap.timeline()
+  var timeline = gsap.timeline({scrollTrigger : {
+    scrub: 2,
+    markers: true,
+    trigger: sideFlwr,
+    start: 'top top',
+    end: 'bottom top'
+    // scroller: ".text",
+  }})
     .to(sideFlwr, {
-      strokeDashoffset: 0, duration: 4000, ease: 'circ.easeIn'
+      strokeDashoffset: 0, duration: 40000, ease: 'circ.easeIn'
     })
-    .fromTo(sideFlwr,{fill: 'none'}, {fill: "#355070"})
+    .fromTo(sideFlwr,{fill: 'transparent'}, { duration: 2, fill: "#355070", ease: "power1"})
+    gsap.to($('.text'), {
+      scrollTrigger: {
+        scrub: 2,
+        autoPlay: true
+      },yPercent: '-50', duration: 20, ease: 'circ.easeOut'}, "<")
 })
