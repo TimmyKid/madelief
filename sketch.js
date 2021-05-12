f0d8499175f8a92e1d802d4ed41f49b45f1490d7
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-12 13:10:15 +0300

    Embroided flowr

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
 const sideFlwr = document.querySelector('#sideFlowr path');
-
 console.log(sideFlwr.getTotalLength())
+
+$(window).on('load', () => {
+  var timeline = gsap.timeline()
+    .to(sideFlwr, {
+      strokeDashoffset: 0, duration: 8, ease: 'circ.easeIn'
+    })
+    .fromTo(sideFlwr,{fill: 'none'}, {fill: "#355070"},"<1")
+})
