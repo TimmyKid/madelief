4ad2ec01627cf6bf4fd17d52e0f2b4b3eb5976a6
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-13 23:09:14 +0300

    sleek preloader

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
 const sideFlwr = document.querySelector('#sideFlowr path');
-console.log(sideFlwr.getTotalLength())
+const loadFlwr = document.querySelector('#loadFlwr path');
+console.log(sideFlwr.getTotalLength() + " " +  loadFlwr.getTotalLength())
+
+var loading = gsap.timeline()
+  .fromTo(loadFlwr,{strokeDashoffset: 26100, fill: 'transparent'},{ strokeDashoffset: 23100, repeat: -1, yoyo: true, duration: 5, fill:'#E56B6F' })
+  .to($('.preload p'),{autoAlpha: .5, yoyo: true, duration: .7, repeat: -1},"<")
+  .to($('.preload'),{autoAlpha: 0, duration: 1.5, ease:"circ.easeOut", delay: 10},"<");
+
+function slideIn() {
+  gsap.from($('svg, .text'),{xPercent: '-10', ease: 'SlowMo.easeOut', duration: 3})
+}
+// loading.pause()
+slideIn()
 
 $(window).on('load', () => {
   var timeline = gsap.timeline({
     scrollTrigger: {
       trigger: '.text',
       start: '-5% top',
-      markers: true,
-      scrub: 1,
+      // markers: true,
+      scrub: 8
     }
   })
     .to($('.text'),{yPercent: '-80', duration: 9, ease: 'power4.easeOut'})
