fbcbb92e6185484631cb26de199fed8fcfba5a3b
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-13 19:05:45 +0300

    Text scrub done

--- sketch.js
+++ sketch.js
@@ -2,26 +2,18 @@ const sideFlwr = document.querySelector('#sideFlowr path');
 console.log(sideFlwr.getTotalLength())
 
 $(window).on('load', () => {
-  var timeline = gsap.timeline({defaults: {
+  var timeline = gsap.timeline({
     scrollTrigger: {
       trigger: '.text',
-      scrub: 2,
+      start: '-5% top',
+      markers: true,
+      scrub: 1,
+      pin: sideFlwr,
+      pinSpacing: false
     }
-  }})
-    .to($('.text'),{yPercent: '-80', duration: 100, ease: 'power4.easeOut'})
-    .to(sideFlwr, {strokeDashoffset: 0, duration: 50, ease: 'power4.easeOut'},"<")
+  })
+    .to($('.text'),{yPercent: '-80', duration: 9, ease: 'power4.easeOut'})
+    .to(sideFlwr, {strokeDashoffset: 0, duration: 80, ease: 'power4.easeOut'},"<")
+    .fromTo(sideFlwr,{ fill: 'transparent' }, { duration: 2, fill: "#d2e3ed", ease:"power4.easeOut"},"<5")
 
-    // .to(sideFlwr, { scrollTrigger: {
-    //   trigger: sideFlwr,
-    //   markers: true,
-    //   pin: true,
-    //   start: "top top",
-    // }, strokeDashoffset: 0, duration: 10, ease: 'circ.easeIn' })
-
-    // .fromTo(sideFlwr,{ fill: 'transparent' }, { duration: 2, fill: "#d2e3ed", ease:"power1"})
-
-    // .to($('.text'), {
-    //   scrollTrigger: {
-    //     scrub: 2,
-    //   },yPercent: '-60', duration: 20, ease: 'circ.easeOut', autoPlay: 'true'}, "<")
 })
