e9791dc61594a91c84c2962b66faa8cf12fe299b
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-15 22:40:41 +0300

    Are we there yet?

--- sketch.js
+++ sketch.js
@@ -14,22 +14,40 @@ function slideIn() {
 slideIn()
 
 $(window).on('load', () => {
-  var audio = document.getElementById('sound')
-  // gsap.set(audio,{muted: false})
-    audio.play()
   var timeline = gsap.timeline({
     scrollTrigger: {
       trigger: '.text',
       start: '-5% top',
-      // markers: true,
-      scrub: 5
-    }
+      scrub: 2,
+      ease: 'sine.easeIn',
+    }, ease:Linear.easeNone
   })
     .to($('.text'),{yPercent: '-80', duration: 9, ease: 'power4.easeOut'})
     .to(sideFlwr, {strokeDashoffset: 0, duration: 80, ease: 'power4.easeOut'},"<")
     .fromTo(sideFlwr,{ fill: 'transparent' }, { duration: 2, fill: "#d2e3ed", ease:"power4.easeOut"},"<5")
 
-  var soundwavr = gsap.timeline({defaults: {stagger: {each: 0.6, repeat: -1}}})
-    .to($('#sound-waves path'),{yPercent: '0', ease: 'sine.easeInOut', duration: 1,yoyo: true, autoAlpha: 0.6, fill: 'grey'})
+  var clickr = 0
+    soundwavr = gsap.timeline({defaults: {yoyo: true, stagger: {each: 0.1, repeat: -1}}, paused:true})
+      .to($('#sound-waves path'),{yPercent: '25', ease:Linear.easeNone, duration: .6, scaleY: .5, fill: '#66695c', reversed: 'true'})
+
+  document.querySelector('#wave').addEventListener('click', function() {
+    var audio = document.getElementById('sound')
+
+    if (clickr == true) {
+      soundwavr.pause();
+      gsap.timeline()
+       .to(audio,{ volume: 0})
+      audio.pause()
+      clickr = 0
+      console.log(clickr + ' paused')
+    } else {
+      soundwavr.play()
+      gsap.to(audio,{ volume: 0.2})
+      audio.play()
+      clickr = 1
+      console.log(clickr + ' playing')
+    }
+  })
+
 
 })
