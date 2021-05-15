cf441b6c5b1508202ed7313e765c73854769d35a
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-05-15 16:27:00 +0300

    audio wave

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 const sideFlwr = document.querySelector('#sideFlowr path');
-const loadFlwr = document.querySelector('#loadFlwr path');
-console.log(sideFlwr.getTotalLength() + " " +  loadFlwr.getTotalLength())
+// const loadFlwr = document.querySelector('#loadFlwr path');
+console.log(sideFlwr.getTotalLength())
 
-var loading = gsap.timeline()
-  .fromTo(loadFlwr,{strokeDashoffset: 26100, fill: 'transparent'},{ strokeDashoffset: 23100, repeat: -1, yoyo: true, duration: 5, fill:'#E56B6F' })
-  .to($('.preload p'),{autoAlpha: .5, yoyo: true, duration: .7, repeat: -1},"<")
-  .to($('.preload'),{autoAlpha: 0, duration: 1.5, ease:"circ.easeOut", delay: 10},"<");
+// var loading = gsap.timeline()
+//   .fromTo(loadFlwr,{strokeDashoffset: 26100, fill: 'transparent'},{ strokeDashoffset: 23100, repeat: -1, yoyo: true, duration: 5 })
+//   .to($('.preload p'),{autoAlpha: .5, yoyo: true, duration: .7, repeat: -1},"<")
+//   .to($('.preload'),{autoAlpha: 0, duration: 1.5, ease:"circ.easeOut", delay: 10},"<");
 
 function slideIn() {
   gsap.from($('svg, .text'),{xPercent: '-10', ease: 'SlowMo.easeOut', duration: 3})
@@ -22,11 +22,14 @@ $(window).on('load', () => {
       trigger: '.text',
       start: '-5% top',
       // markers: true,
-      scrub: 8
+      scrub: 5
     }
   })
     .to($('.text'),{yPercent: '-80', duration: 9, ease: 'power4.easeOut'})
     .to(sideFlwr, {strokeDashoffset: 0, duration: 80, ease: 'power4.easeOut'},"<")
     .fromTo(sideFlwr,{ fill: 'transparent' }, { duration: 2, fill: "#d2e3ed", ease:"power4.easeOut"},"<5")
+
+  var soundwavr = gsap.timeline({defaults: {stagger: {each: 0.6, repeat: -1}}})
+    .to($('#sound-waves path'),{yPercent: '0', ease: 'sine.easeInOut', duration: 1,yoyo: true, autoAlpha: 0.6, fill: 'grey'})
 
 })
